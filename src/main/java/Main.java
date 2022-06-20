5b01f047b54f7c0b343b5a41f0d5dcec05bc5e31
olgaobraztsova <olga.obraztsova@gmail.com>
2022-06-20 17:13:22 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Ints intsCalc = new IntsCalculator(new Calculator());
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.mult(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+
+    }
+}
